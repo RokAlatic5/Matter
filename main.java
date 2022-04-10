5039c12d39225e181c94a2dbc50981e166bcdbd2
RokAlatic <45037112+RokAlatic5@users.noreply.github.com>
2022-04-10 11:56:13 +0200

    Add files via upload

--- main.java
+++ main.java
@@ -1,27 +1,73 @@
+import com.jagrosh.jdautilities.command.CommandBuilder;
+import com.jagrosh.jdautilities.command.CommandClientBuilder;
+import com.jagrosh.jdautilities.commons.waiter.EventWaiter;
 import net.dv8tion.jda.api.*;
 import net.dv8tion.jda.api.entities.*;
+import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.exceptions.RateLimitedException;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.components.buttons.Button;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.utils.cache.CacheFlag;
+import org.ini4j.Ini;
 import org.jetbrains.annotations.NotNull;
 
 import javax.security.auth.login.LoginException;
+import java.io.File;
+import java.io.IOException;
 import java.sql.*;
 import java.lang.String;
 
-public class main extends ListenerAdapter {
+public class main {
 
+    static String token;
+    static String owner;
+    static String prefix;
+    static String url;
+    static String username;
+    static String password;
 
     public static void main(String[] args)
-            throws LoginException {
-        JDA jda = JDABuilder.createDefault(hidden.token, GatewayIntent.GUILD_MESSAGES, GatewayIntent.GUILD_MEMBERS, GatewayIntent.GUILD_PRESENCES)
-                .enableCache(CacheFlag.ONLINE_STATUS, CacheFlag.ACTIVITY, CacheFlag.CLIENT_STATUS).build();
-        jda.addEventListener(new main());
+            throws LoginException, IOException, RateLimitedException {
+        Ini config = new Ini( new File("C:/Users/Home/Desktop/MatterBot/config.ini"));
+
+        token = config.get("botStuff", "botToken");
+        owner = config.get("botStuff", "botOwner");
+        prefix = config.get("botStuff", "botPrefix");
+        url = config.get("database", "url");
+        username = config.get("database", "username");
+        password = config.get("database", "password");
+
+        EventWaiter waiter = new EventWaiter();
+
+        CommandClientBuilder bot = new CommandClientBuilder();
+
+        bot.useDefaultGame();
+
+        bot.setOwnerId(owner);
+
+        bot.setPrefix(prefix);
+
+        bot.addCommands(
+                new psn(),
+                new setpsn(),
+                new warn(),
+                new appeal(),
+                new userInfo()
+        );
+
+
+        JDABuilder.createDefault(token)
+                .addEventListeners(waiter, bot.build())
+                .setStatus(OnlineStatus.DO_NOT_DISTURB)
+                .setActivity(Activity.playing("loading..."))
+
+                .build();
 
         System.out.println("[Matter] Connecting database...");
 
-        try (Connection connection = DriverManager.getConnection(hidden.url, hidden.username, hidden.password)) {
+        try (Connection connection = DriverManager.getConnection(url, username, password)) {
             System.out.println("[Matter] Database connected!");
 
             connection.close();
@@ -31,29 +77,5 @@ public class main extends ListenerAdapter {
             throw new IllegalStateException("[Matter] Cannot connect the database!", e);
         }
     }
-
-    @Override
-    public void onMessageReceived(@NotNull MessageReceivedEvent event) {
-        Message msg = event.getMessage();
-        MessageChannel channel = event.getChannel();
-        Member member = event.getMember();
-        if (!event.getAuthor().isBot()) {
-
-            if (msg.getContentRaw().startsWith("!setpsn")) {
-                setpsn.setUserPsn(event);
-            }
-            if (msg.getContentRaw().startsWith("!psn")) {
-                psn.userPsn(event);
-            }
-            if (msg.getContentRaw().startsWith("!userinfo")) {
-                    userInfo.userInformation(event);
-            }
-            if (msg.getContentRaw().startsWith("!warn")) {
-                warn.warnUser(event);
-            }
-            if (msg.getContentRaw().startsWith("!appeal")) {
-                appeal.appeal(event);
-            }
-        }
-    }
 }
+
