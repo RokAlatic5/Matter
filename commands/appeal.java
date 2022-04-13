e054c5cd6deebded438db85a71c7486de3d390a7
RokAlatic <45037112+RokAlatic5@users.noreply.github.com>
2022-04-13 22:11:50 +0200

    Add files via upload

--- commands/appeal.java
+++ commands/appeal.java
@@ -1,47 +1,106 @@
 import com.jagrosh.jdautilities.command.Command;
 import com.jagrosh.jdautilities.command.CommandEvent;
+import com.jagrosh.jdautilities.commons.waiter.EventWaiter;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.*;
+import net.dv8tion.jda.api.events.GenericEvent;
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
-import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import net.dv8tion.jda.api.interactions.components.ActionRow;
 import net.dv8tion.jda.api.interactions.components.buttons.Button;
-import net.dv8tion.jda.api.interactions.components.buttons.ButtonInteraction;
 import org.jetbrains.annotations.NotNull;
 
 import java.sql.*;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-public class appeal extends Command{
+public class appeal extends Command implements net.dv8tion.jda.api.hooks.EventListener {
 
-    public appeal() {
+     private final EventWaiter waiter;
+
+
+    appeal(EventWaiter waiter) {
         this.name = "appeal";
         this.help = "Appeals for a punishement";
         this.botPermissions = new Permission[]{};
         this.guildOnly = false;
+        this.waiter = waiter;
     }
 
-    private final long channelId, authorId;
+    private String AppealReason;
+    private static String Warn;
+    private static String Punisher;
+    private static String AppealID;
+    private static String UserID;
+    private static String GuildId;
 
-    static String Warn;
-    static String Punisher;
+    private static String buttonID;
+    private static Button accept;
 
-    public appeal(MessageChannel channel, User author) {
-        this.channelId = channel.getIdLong();
-        this.authorId = author.getIdLong();
+
+    private void waitForReason(CommandEvent event) {
+        waiter.waitForEvent(MessageReceivedEvent.class,
+                e -> e.getAuthor().equals(event.getAuthor())
+                        && e.getChannel().equals(event.getChannel())
+                        && !e.getMessage().equals(event.getMessage()),
+                e -> { event.reply("Please attach images if you have any or type none");
+                    AppealReason = e.getMessage().getContentDisplay();
+                    System.out.println(AppealReason);
+                    accept = Button.success(AppealID+","+UserID+","+GuildId, "Accept");
+                    waitForAttachment(event);
+                }
+                ,
+                5, TimeUnit.MINUTES, () -> event.reply("Appeal submit timed out."));
     }
 
+
+
+    private void waitForAttachment(CommandEvent event) {
+        waiter.waitForEvent(MessageReceivedEvent.class,
+                e -> e.getAuthor().equals(event.getAuthor())
+                        && e.getChannel().equals(event.getChannel())
+                        && !e.getMessage().equals(event.getMessage()),
+                e -> {String response = e.getMessage().getContentDisplay();
+                    if (response.equals("none")) {
+                        List attachments = e.getMessage().getAttachments();
+                        for (int i = 0; i < attachments.size(); i++) {
+                            Message.Attachment attachment = e.getMessage().getAttachments().get(i);
+                            event.reply(attachment.getUrl());
+                        }
+                    } else if (response.isEmpty()) {
+                        List attachments = e.getMessage().getAttachments();
+                        for (int i = 0; i < attachments.size(); i++) {
+                            Message.Attachment attachment = e.getMessage().getAttachments().get(i);
+                            event.reply(attachment.getUrl());
+                        }
+                    }
+                    submitAppeal(event);
+                },
+                2, TimeUnit.MINUTES, () -> event.reply("Appeal submit timed out."));
+
+    }
+
+    private final Button reject = Button.danger("reject", "Reject");
+
+    private void submitAppeal(CommandEvent e) {
+        EmbedBuilder embed = new EmbedBuilder();
+        embed.setTitle(e.getMember().getUser().getName() + " is appealing for their punishment.");
+        embed.addField("Warning:", Warn, false);
+        embed.addField("Moderator: ", Punisher, false);
+        embed.addField("Reason: ", AppealReason , false);
+        e.getChannel().sendMessageEmbeds(embed.build()).setActionRow(
+                accept, reject)
+                .queue();
+
+    }
+
+
+
+@Override
     protected void execute(CommandEvent event) {
+    UserID = event.getMember().getId();
+    GuildId = event.getGuild().getId();
         int msgLength = event.getMessage().getContentDisplay().split(" ").length;
-        String UserID = event.getMember().getId();
-        String channelId = event.getChannel().getId();
-        String AppealID;
-        Member member = event.getMember();
-        String GuildId = event.getGuild().getId();
         final MessageChannel channel = event.getChannel();
         if (msgLength <= 1) {
             channel.sendMessage("You need to enter your appeal ID").queue();
@@ -53,7 +112,7 @@ public class appeal extends Command{
 
             Statement statement = connection.createStatement();
 
-            String checkForAppeals = "SELECT Warn1 , Warn2 , Warn3, Executor , Executor1 ,Executor2 ,Appeal1, Appeal2, Appeal3 FROM Warn WHERE UserID='"+UserID+"' AND Guild='"+GuildId+"'";
+            String checkForAppeals = "SELECT Warn1 , Warn2 , Warn3, Executor , Executor1 ,Executor2 ,Appeal1, Appeal2, Appeal3 FROM Warn WHERE UserID='" + UserID + "' AND Guild='" + GuildId + "'";
 
 
             ResultSet rs = statement.executeQuery(checkForAppeals);
@@ -96,13 +155,12 @@ public class appeal extends Command{
 
 
             if (AppealID.equals(Appeal1) || AppealID.equals(Appeal2) || AppealID.equals(Appeal3)) {
-                channel.sendMessage("Please explain why you want to appeal this punishment.").queue();
-                event.getJDA().addEventListener(new appeal(channel, member.getUser()));
+                channel.sendMessage("Please explain why this punishment should be appealed.").queue();
+                waitForReason(event);
             } else {
                 channel.sendMessage("Unknown punishment ID").queue();
                 return;
             }
-
 
 
             connection.close();
@@ -114,50 +172,59 @@ public class appeal extends Command{
 
     }
 
-
-    @Override
-    public void onMessageReceived(MessageReceivedEvent event) {
-        if (event.getAuthor().isBot()) return;
-        if (event.getChannel().getIdLong() != channelId) return;
-        if (event.getAuthor().getIdLong() == authorId) {
-            MessageChannel channel = event.getChannel();
-            String content = event.getMessage().getContentRaw();
-            Message.Attachment attachment;
-            String AppealReason = "";
-            String Attachment = "";
-            if (Attachment.isEmpty() || AppealReason.isEmpty()) {
-            if (content.equalsIgnoreCase("cancel") || content.equalsIgnoreCase("stop")) {
-                channel.sendMessage("Appeal request canceled!").queue();
-                event.getJDA().removeEventListener(this);
+    public void onEvent(@NotNull GenericEvent event) {
+        if (event instanceof ButtonInteractionEvent) {
+            if (((ButtonInteractionEvent) event).getComponentId().equals("reject")) {
+                ((ButtonInteractionEvent) event).reply("Appeal Declined.");
             }
-                if (content.isEmpty() == false) {
-                    AppealReason = event.getMessage().getContentDisplay();
-                    channel.sendMessage("Please include photos or type no if you have none.").queue();
-                }
+            else {
+                buttonID = ((ButtonInteractionEvent) event).getComponentId();
+                ((ButtonInteractionEvent) event).reply("Appeal accepted.");
                 try {
-                    if (content.isEmpty() == true || event.getMessage().getAttachments().get(0) != null) {
-                        Attachment = event.getMessage().getAttachments().get(0).getUrl();
-                        System.out.println(Attachment);
-                        EmbedBuilder embed = new EmbedBuilder();
-                        embed.setTitle(event.getMember().getUser().getName() + " is appealing for their punishment.");
-                        embed.addField("Warning:", Warn, false);
-                        embed.addField("Moderator: ", Punisher, false);
-                        embed.addField("Reason: ", AppealReason, false);
-                        channel.sendMessageEmbeds(embed.build()).setActionRow(
-                                Button.primary("hello", "Click Me")) // Button with only an emoji
-                                .queue();
-                        for (int i = 0; i < event.getMessage().getAttachments().size(); i++)
-                            channel.sendMessage(event.getMessage().getAttachments().get(i).getUrl()).queue();
+                    Connection connection = DriverManager.getConnection(main.url, main.username, main.password);
+
+                    Statement statement = connection.createStatement();
+
+                    String args[] = buttonID.split(",");
+
+                    String query = "SELECT Warns, Appeal1, Appeal2, Appeal3 FROM Warn WHERE UserID='"+args[1]+"' AND Guild='"+args[2]+"'";
+
+                    ResultSet rs = statement.executeQuery(query);
+
+                    String Appeal1 = "";
+                    String Appeal2 = "";
+                    String Appeal3 = "";
+                    int Warns = 0;
+
+                    while (rs.next()) {
+                        Appeal1 = rs.getString("Appeal1");
+                        Appeal2 = rs.getString("Appeal2");
+                        Appeal3 = rs.getString("Appeal3");
+                        Warns = rs.getInt("Warns");
                     }
-                } catch (IndexOutOfBoundsException e) { }
+
+                    Warns = Warns - 1;
+
+                    if (Appeal1.equals(args[0])) {
+                        System.out.println("This is Warning 1");
+                        String updatePSN = "UPDATE Warn " +
+                                "SET Warn1 = 'None', " +
+                                "SET Appeal1 = 'None'," +
+                                "SET Executor"
+                                "WHERE UserID='"+args[1]+"' AND Guild='"+args[2]+"'";
+                    } else if (Appeal2.equals(args[0])) {
+                        System.out.println("This is Warning 2");
+                        ((ButtonInteractionEvent) event).reply("This is warning number 2");
+                    } else if (Appeal3.equals(args[0])) {
+                        System.out.println("This is Warning 3");
+                        ((ButtonInteractionEvent) event).reply("This is warning number 3");
+                    }
+
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+
             }
-        }
-    }
-    @Override
-    public void onButtonInteraction(@NotNull ButtonInteractionEvent interaction) {
-        System.out.println("test");
-        if (interaction.getComponentId().equals("hello")) {
-            interaction.reply("Hello :)").queue(); // send a message in the channel
         }
     }
 }
